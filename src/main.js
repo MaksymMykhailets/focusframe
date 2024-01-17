3cb81a88c903a79529f83e232f17d22899b89977
sauergru <sauergru@gmail.com>
2024-01-17 16:40:44 +0100

    updated

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-'use strict';
-{
-  let menuBtnOpen = document.querySelector('.burger-button');
-  menuBtnOpen.addEventListener('click', function () {
-    mobileMenuToggle();
-  });
+// 'use strict';
+// {
+//   let menuBtnOpen = document.querySelector('.burger-button');
+//   menuBtnOpen.addEventListener('click', function () {
+//     mobileMenuToggle();
+//   });
 
-  let menuBtnClose = document.querySelector('.button-close');
-  menuBtnClose.addEventListener('click', function () {
-    mobileMenuToggle();
-  });
+//   let menuBtnClose = document.querySelector('.button-close');
+//   menuBtnClose.addEventListener('click', function () {
+//     mobileMenuToggle();
+//   });
 
-  let menu = document.querySelector('.backdrop');
-  function mobileMenuToggle() {
-    menu.classList.toggle('active');
-  }
-}
+//   let menu = document.querySelector('.backdrop');
+//   function mobileMenuToggle() {
+//     menu.classList.toggle('active');
+//   }
+// }
