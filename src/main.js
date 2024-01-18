98af192de8feeba1ea9d3a17797ef273df4f819f
vButkevich <vyacheslav.butkevich@gmail.com>
2024-01-18 12:43:11 +0200

    js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
-// 'use strict';
-// {
-//   let menuBtnOpen = document.querySelector('.burger-button');
-//   menuBtnOpen.addEventListener('click', function () {
-//     mobileMenuToggle();
-//   });
+'use strict';
+{
+  let menuBtnOpen = document.querySelector('.header-menu-btn');
+  menuBtnOpen.addEventListener('click', function () {
+    mobileMenuToggle();
+  });
 
-//   let menuBtnClose = document.querySelector('.button-close');
-//   menuBtnClose.addEventListener('click', function () {
-//     mobileMenuToggle();
-//   });
+  let menuBtnClose = document.querySelector('.mobile-button');
+  menuBtnClose.addEventListener('click', function () {
+    mobileMenuToggle();
+  });
 
-//   let menu = document.querySelector('.backdrop');
-//   function mobileMenuToggle() {
-//     menu.classList.toggle('active');
-//   }
-// }
+  let menuItems = document.querySelectorAll('.menu-item');
+  for (let menuItem of menuItems) {
+    menuItem.addEventListener('click', function () {
+      mobileMenuToggle();
+    });
+  }
+
+  let menu = document.querySelector('.backdrop');
+  function mobileMenuToggle() {
+    menu.classList.toggle('active');
+  }
+}
